20efc82a72f31a320ef5238acc1b750b1777e027
Cédric GNIEWEK <gniewek.cedric@free.fr>
2021-06-04 11:46:19 +0200

    Add CAB sign without a signal

---
+++ TVM430_Cab.cs
@@ -0,0 +1,35 @@
+using Orts.Simulation.Signalling;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ORTS.Scripting.Script
+{
+    public class TVM430_Cab : CsSignalScript
+    {
+        public TVM430_Cab()
+        {
+        }
+
+        public override void Initialize()
+        {
+        }
+
+        public override void Update()
+        {
+            int nextNormalSignalId = NextSignalId("NORMAL");
+            Aspect nextNormalSignalMstsAspect = nextNormalSignalId >= 0 ? IdSignalAspect(nextNormalSignalId, "NORMAL") : Aspect.Stop;
+            string nextNormalSignalTextAspect = nextNormalSignalId >= 0 ? IdTextSignalAspect(nextNormalSignalId, "NORMAL") : "EOA";
+            List<string> nextNormalParts = nextNormalSignalTextAspect.Split(' ').ToList();
+            nextNormalSignalTextAspect = string.Join(" ", nextNormalParts.Where(x =>
+                x.StartsWith("FR_TVM")
+                || x.StartsWith("Ve")
+                || x.StartsWith("Vc")
+                || x.StartsWith("Va")));
+
+            MstsSignalAspect = nextNormalSignalMstsAspect;
+            TextSignalAspect = nextNormalSignalTextAspect + " BSP_ECS";
+
+            DrawState = DefaultDrawState(MstsSignalAspect);
+        }
+    }
+}
