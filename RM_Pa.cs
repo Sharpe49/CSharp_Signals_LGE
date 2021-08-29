c6c090d50fa97755174c4a6642f2a2ed01dcd2ce
Cédric GNIEWEK <gniewek.cedric@free.fr>
2021-08-29 14:25:30 +0200

    Added ability to use junction links for P signs

--- RM_Pa.cs
+++ RM_Pa.cs
@@ -23,6 +23,19 @@ namespace ORTS.Scripting.Script
                 MstsSignalAspect = Aspect.Clear_2;
                 TextSignalAspect = "FR_TABP_EFFACE";
             }
+            else if (IsSignalFeatureEnabled("USER1"))
+            {
+                if (RouteSet)
+                {
+                    MstsSignalAspect = Aspect.Clear_2;
+                    TextSignalAspect = "FR_TABP_EFFACE";
+                }
+                else
+                {
+                    MstsSignalAspect = Aspect.Approach_1;
+                    TextSignalAspect = "FR_TABP_PRESENTE";
+                }
+            }
             // Tableau P présenté
             else if (nextNormalParts.Contains("FR_C_BAL")
                 || nextNormalParts.Contains("FR_S_BAL")
